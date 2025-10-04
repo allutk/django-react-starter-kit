73831e6dd2c258663f67fa6a79b827a3d4b9239d
allutk <aleksei.lutkov@yahoo.com>
2025-10-04 21:04:10 +0300

    Backend auth functionality

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,22 @@
+import uuid
 from django.db import models
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 
-# Create your models here.
+from .managers import UserManager
+
+
+class User(AbstractBaseUser, PermissionsMixin):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    email = models.EmailField(max_length=200, unique=True)
+    first_name = models.CharField(max_length=200, null=True)
+    last_name = models.CharField(max_length=200, null=True)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+
+    USERNAME_FIELD = 'email'
+
+    objects = UserManager()
+
+    def __str__(self):
+        return self.email
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,7 +1,56 @@
-from rest_framework.decorators import api_view
+from rest_framework import generics, status
+from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
+from rest_framework.request import Request
+from rest_framework.views import APIView
+
+from .models import User
+from .serializers import (UserPasswordUpdateSerializer,
+                          UserSerializer,
+                          UserUpdateSerializer)
 
 
-@api_view()
-def ping(request):
-    return Response({"message": "pong"})
+class UserListCreateView(generics.ListCreateAPIView):
+    serializer_class = UserSerializer
+
+    def get_queryset(self):
+        return User.objects.filter(email=self.request.user)
+
+    def get_permissions(self):
+        self.permission_classes = [AllowAny]
+        if self.request.method == 'GET':
+            self.permission_classes = [IsAuthenticated]
+        return super().get_permissions()
+
+    def list(self, _: Request):
+        serializer = UserSerializer(self.get_queryset(), many=True)
+        return Response(serializer.data[0])
+
+    def perform_create(self, serializer: UserSerializer):
+        serializer.save()
+
+
+class UserUpdateView(generics.UpdateAPIView):
+    serializer_class = UserUpdateSerializer
+    lookup_url_kwarg = 'id'
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return User.objects.filter(email=self.request.user)
+
+
+class UserPasswordUpdateView(APIView):
+    serializer_class = UserPasswordUpdateSerializer
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request: Request):
+        serializer = UserPasswordUpdateSerializer(
+            data=request.data,
+            context={ 'request': request }
+        )
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'detail': 'Password changed successfully'},
+                            status=status.HTTP_200_OK)
+        return Response(serializer.errors,
+                        status=status.HTTP_400_BAD_REQUEST)
